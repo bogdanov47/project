0fb381b34819c2c68e4cf8000ff993cb16e8e0a3
Alexey <example@gmail.com>
2020-12-06 14:35:28 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Alexey';
+alert(`Привет, ${user}`);
+
+let incr =  10,
+    decr =  10;
+
+    incr++;
+    decr--;
+    console.log(incr);
+    console.log(decr);
+
+    console.log(3*7 == 21);
