63cdc12a2f2ab594476e8230f86f92c9e21c624c
Alexey <example@gmail.com>
2020-12-06 18:07:47 +0300

    first prilogenie

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const NumberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const PerosnalMovieDB = {
+    count: NumberOfFilms,
+    movies: {},
+    actors: {},
+    ganres: [],
+    privat: false
+};
+
+const   a = prompt('Один из последних просмотренных фильмов?',''),
+        b = prompt('На сколько оцените его?',''),
+        c = prompt('Один из последних просмотренных фильмов?',''),
+        d = prompt('На сколько оцените его?','');
+
+        PerosnalMovieDB.movies[a] = b;
+        PerosnalMovieDB.movies[c] = d;
+
+        console.log(PerosnalMovieDB);
