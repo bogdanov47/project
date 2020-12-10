9166214518c6bcdbfd1fd63ff8f3c2a2dfc6d20c
Alexey <example@gmail.com>
2020-12-11 01:15:47 +0300

    objectandkey

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-function learnJS(lang, callback){
-    console.log(`Я учу ${lang}`);
-    callback();
+const options = {
+    name: 'test',
+    width: 1024,
+    hight: 1024,
+    color:{
+        border: 'black',
+        bg: 'red'
+    }
+};
+console.log(options.name);
+
+for (let key in options){
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    }else{
+        console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    }
+    
 }
-function done (){
-    console.log('Я прошел этот урок');
-}
-learnJS('Java script',done);
