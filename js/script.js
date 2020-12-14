588827397bbee72611768cf0cd0147ab8070c831
Alexey <example@gmail.com>
2020-12-15 00:46:19 +0300

    OOP

--- js/script.js
+++ js/script.js
@@ -1,62 +1,22 @@
 'use strict';
-// const obj = {
-//     a: 1,
-//     b: 7
-// // };
-// function copy(mainObj){
-//     let objCopy = {};
-//     let key;
-//     for (key in mainObj){
-//         objCopy[key] = mainObj[key];
-//     }
-//     return objCopy;
-// }
-// const num = {
-//     a: 5,
-//     b: 4,
-//     c:{
-//         x:4,
-//         y:7
-//     }
+const str = 'some';
+const strObj = new String(str); 
+console.log(typeof(str));
+console.log(typeof(strObj));
+
+
+const solider = {
+    health: 400,
+    armor: 100,
+    sayHello : function(){
+        console.log('hello');
+    }
+
+};
+const john = Object.create(solider);
+// const john ={
+//     health: 100
 // };
-// const newNum = copy(num);
-// newNum.a = 10;
-// console.log(num);
-// console.log(newNum);
-
-const add = {
-    d:18,
-    j:57
-};
-const clone = (Object.assign({}, add));
-clone.d = 53;
-console.log(add);
-console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-newArray[2] = 'poshel nahyu pes';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-      console.log(internet);
-
-      function log(a, b ,c){
-         console.log(a);
-         console.log(b);
-         console.log(c);
-      }
-      const num = [2, 5, 7];
-console.log(...num);
-
-const array = ['a', 'b'];
-const neaAraay = [...array];
-
-const q = {
-    one: 1,
-    two: 2
-};
-const newObj = {...q};
+// john.__proto__ = solider;
+// Object.setPrototypeOf(john,solider);
+john.sayHello();
