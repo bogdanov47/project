d05c0a6d790658e34aa5c812b7df218d99cfc912
Alexey <example@gmail.com>
2020-12-14 21:01:50 +0300

    Spread operator

--- js/script.js
+++ js/script.js
@@ -1,30 +1,62 @@
 'use strict';
+// const obj = {
+//     a: 1,
+//     b: 7
+// // };
+// function copy(mainObj){
+//     let objCopy = {};
+//     let key;
+//     for (key in mainObj){
+//         objCopy[key] = mainObj[key];
+//     }
+//     return objCopy;
+// }
+// const num = {
+//     a: 5,
+//     b: 4,
+//     c:{
+//         x:4,
+//         y:7
+//     }
+// };
+// const newNum = copy(num);
+// newNum.a = 10;
+// console.log(num);
+// console.log(newNum);
 
-const options = {
-    name: 'test',
-    width: 1024,
-    hight: 1024,
-    color:{
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function(){
-    console.log('test'); 
-    }
+const add = {
+    d:18,
+    j:57
 };
-options.makeTest();
-console.log(options.name);
+const clone = (Object.assign({}, add));
+clone.d = 53;
+console.log(add);
+console.log(clone);
 
-for (let key in options){
-    if (typeof(options[key]) === 'object'){
-        for (let i in options[key]){
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-        }
-    }else{
-        console.log(`Свойство ${key} имеет значение ${options[key]}`)
-    }
-    
-}
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[2] = 'poshel nahyu pes';
+console.log(newArray);
+console.log(oldArray);
 
-// console.log(Object.keys(options).length);
-const {border, bg} = options.colors;
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+      console.log(internet);
+
+      function log(a, b ,c){
+         console.log(a);
+         console.log(b);
+         console.log(c);
+      }
+      const num = [2, 5, 7];
+console.log(...num);
+
+const array = ['a', 'b'];
+const neaAraay = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+const newObj = {...q};
