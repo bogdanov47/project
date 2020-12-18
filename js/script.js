a65ed6465f94ad3c541ad58aa6a1d3f23c70eebc
Alexey <example@gmail.com>
2020-12-19 02:05:40 +0300

    gg wp

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
 'use strict';
-const str = 'some';
-const strObj = new String(str); 
-console.log(typeof(str));
-console.log(typeof(strObj));
+//to string
+//  1) Old version
+// console.log(typeof(string(null)));
 
+// console.log(typeof(string(5)));
 
-const solider = {
-    health: 400,
-    armor: 100,
-    sayHello : function(){
-        console.log('hello');
-    }
+//  2)
+console.log(typeof(5 + ''));
 
-};
-const john = Object.create(solider);
-// const john ={
-//     health: 100
-// };
-// john.__proto__ = solider;
-// Object.setPrototypeOf(john,solider);
-john.sayHello();
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 24 + 'px';
+
+//to number
+
+// 1)
+
+console.log(typeof(Number('5')));
+
+// 2)
+
+console.log(typeof(+'5'));
+
+//3)
+
+console.log(typeof(parseInt('15px', 10)));
+
+let answ = +prompt('hello', '');
+
+//to boolean
+
+// 0, '', null, undefined, NaN;   Всегда false
+
+// 1)
+let switcher = null;
+if (switcher){
+    console.log('Working...');
+}
+switcher = 1;
+
+if (switcher){
+    console.log('Working...');
+}
+
+// 2)
+
+console.log(typeof(Boolean('5')));
+
+// 3)
+
+console.log(typeof(!!'444444'));
